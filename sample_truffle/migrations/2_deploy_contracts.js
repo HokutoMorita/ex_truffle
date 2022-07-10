91e036a62c48bfd4feef954b61ecf10e02338abd
Hokuto Morita <han2210mh@gmail.com>
2022-07-10 14:26:29 +0900

    [add]デバッグ確認用のコントラクトの追加

--- sample_truffle/migrations/2_deploy_contracts.js
+++ sample_truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("SimpleStorage");
+var Fibonacci = artifacts.require("Fibonacci");
 
 module.exports = function (deployer) {
     deployer.deploy(SimpleStorage);
+    deployer.deploy(Fibonacci);
 };
