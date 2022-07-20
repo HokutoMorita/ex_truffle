7a73171ee0dd71f68f1ab16f2f83224c91b3d13b
Hokuto Morita <han2210mh@gmail.com>
2022-07-20 22:02:47 +0900

    [clean]マイグレーションをUUPSに対応させる

--- pet-shop-tutorial/migrations/2_deploy_contracts.js
+++ pet-shop-tutorial/migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ var AdoptionV3 = artifacts.require("AdoptionV3");
 const floorPrice = web3.utils.toWei("0.005", "ether");
 
 module.exports = async function(deployer) {
-    const adoption = await deployProxy(Adoption, [floorPrice], { deployer, initializer: 'initialize' });
+    const adoption = await deployProxy(Adoption, [floorPrice], { deployer, kind: 'uups', initializer: 'initialize' });
     const adoption2 = await upgradeProxy(adoption.address, AdoptionV2);
     const callInitialize = { call: { fn: 'setFloorPrice2', args: [floorPrice] } }
     await upgradeProxy(adoption2.address, AdoptionV3, callInitialize);
